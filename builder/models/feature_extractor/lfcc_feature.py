9eb428a1ad7a1ef43fa7388fcecd5c1680306525
Jaskirat <jjaskirat.ssingh@gmail.com>
2023-11-30 22:21:05 +0530

    uploaded project files

---
+++ builder/models/feature_extractor/lfcc_feature.py
@@ -0,0 +1,51 @@
+import platform
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch import Tensor, FloatTensor
+import matplotlib.pyplot as plt
+from scipy import signal as sci_sig
+import torchaudio
+
+class LFCC_FEATURE(nn.Module):
+    def __init__(self,
+            sample_rate: int = 200,
+            frame_length: int = 16,
+            frame_shift: int = 8,
+            feature_extract_by: str = 'kaldi'):
+        super(LFCC_FEATURE, self).__init__()
+
+        self.sample_rate = sample_rate
+        self.feature_extract_by = feature_extract_by.lower()
+        self.freq_resolution = 2
+
+        assert platform.system().lower() == 'linux' or platform.system().lower() == 'darwin'
+        self.transforms = torchaudio.transforms.LFCC(sample_rate = 200, 
+                                    n_filter = 32,
+                                    f_min = 0.0,    # 0 Hz ~
+                                    f_max = 100,     # ~ 60Hz
+                                    n_lfcc = 8,    # 8 coefficietns
+                                    dct_type = 2, 
+                                    norm = 'ortho', 
+                                    log_lf = False,
+                                    speckwargs = {"n_fft":400, "win_length":60, "hop_length":30}) # 300miliseconds --> 60 points for 200Hz, 150miliseconds --> 30 points for 200Hz
+
+    def psd(self, amp, begin, end):
+        return torch.mean(amp[begin*self.freq_resolution:end*self.freq_resolution], 0)
+        
+    def forward(self, batch):
+        final_batch = []
+
+        for signals in batch:
+            transformed_sample = []
+            for signal in signals:
+                stft = self.transforms(signal)
+                amp = (torch.log(torch.abs(stft) + 1e-10))
+                    
+                transformed_sample.append(amp)
+
+            final_batch.append(torch.stack(transformed_sample))
+
+        return torch.stack(final_batch)
+
---
+++ builder/models/feature_extractor/spectrogram_feature_binary.py
@@ -0,0 +1,109 @@
+import platform
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch import Tensor, FloatTensor
+import matplotlib.pyplot as plt
+from scipy import signal as sci_sig
+# this spectrogram setting is specifically for alexnet eeg paper
+
+class SPECTROGRAM_FEATURE_BINARY1(nn.Module):
+    def __init__(self,
+            sample_rate: int = 200,
+            frame_length: int = 16,
+            frame_shift: int = 8,
+            feature_extract_by: str = 'kaldi'):
+        super(SPECTROGRAM_FEATURE_BINARY1, self).__init__()
+
+        self.sample_rate = sample_rate
+        self.feature_extract_by = feature_extract_by.lower()
+        self.freq_resolution = 1
+
+        if self.feature_extract_by == 'kaldi':
+            assert platform.system().lower() == 'linux' or platform.system().lower() == 'darwin'
+            import torchaudio
+
+            self.transforms = torchaudio.transforms.Spectrogram(n_fft=self.freq_resolution*self.sample_rate,
+                                                                win_length=frame_length,
+                                                                hop_length=frame_shift)
+
+        else:
+            self.n_fft = self.freq_resolution*self.sample_rate
+            self.hop_length = frame_shift
+            self.frame_length = frame_length
+        
+    def forward(self, batch):
+        spectrogram_batch = []
+
+        for signals in batch:
+            spectrogram_sample = []
+            for signal in signals:
+                if self.feature_extract_by == 'kaldi':
+                    stft = self.transforms(signal)
+                    amp = (torch.log(torch.abs(stft) + 1e-10))
+
+                else:
+                    stft = torch.stft(
+                        signal, self.n_fft, hop_length=self.hop_length,
+                        win_length=self.frame_length, window=torch.hamming_window(self.frame_length),
+                        center=False, normalized=False, onesided=True
+                    )
+                    
+                    amp = (torch.log(torch.abs(stft) + 1e-10))
+    
+                spectrogram_sample.append(amp[:50,:])
+
+            spectrogram_batch.append(torch.stack(spectrogram_sample))
+
+        return torch.stack(spectrogram_batch)
+
+class SPECTROGRAM_FEATURE_BINARY2(nn.Module):
+    def __init__(self,
+            sample_rate: int = 200,
+            frame_length: int = 16,
+            frame_shift: int = 8,
+            feature_extract_by: str = 'kaldi'):
+        super(SPECTROGRAM_FEATURE_BINARY2, self).__init__()
+
+        self.sample_rate = sample_rate
+        self.feature_extract_by = feature_extract_by.lower()
+        self.freq_resolution = 1
+
+        if self.feature_extract_by == 'kaldi':
+            assert platform.system().lower() == 'linux' or platform.system().lower() == 'darwin'
+            import torchaudio
+
+            self.transforms = torchaudio.transforms.Spectrogram(n_fft=self.freq_resolution*self.sample_rate,
+                                                                win_length=frame_length,
+                                                                hop_length=frame_shift)
+
+        else:
+            self.n_fft = self.freq_resolution*self.sample_rate
+            self.hop_length = frame_shift
+            self.frame_length = frame_length
+        
+    def forward(self, batch):
+        spectrogram_batch = []
+
+        for signals in batch:
+            spectrogram_sample = []
+            for signal in signals:
+                if self.feature_extract_by == 'kaldi':
+                    stft = self.transforms(signal)
+                    amp = (torch.log(torch.abs(stft) + 1e-10))
+                else:
+                    stft = torch.stft(
+                        signal, self.n_fft, hop_length=self.hop_length,
+                        win_length=self.frame_length, window=torch.hamming_window(self.frame_length),
+                        center=False, normalized=False, onesided=True
+                    )
+                    
+                    amp = (torch.log(torch.abs(stft) + 1e-10))
+    
+                spectrogram_sample.append(amp[:100,:])
+
+            spectrogram_batch.append(torch.stack(spectrogram_sample))
+
+        return torch.stack(spectrogram_batch)
+
